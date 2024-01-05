b007721e353a2f18651796b3118245ae063bd02a
anatolie ghebea <anatolieghebea@gmail.com>
2024-01-05 17:46:26 +0100

    found combo

--- src/1dcutopt/1DCutOpt.svelte
+++ src/1dcutopt/1DCutOpt.svelte
@@ -5,6 +5,11 @@
     let groups = [];
     let mounted = false;
     let matrix_map = [];
+    let flattened_map = [];
+    let grouped_map = [];
+    const availableSets = [
+        { name: 'default', len: 6500, default_loss: 200 }
+    ]
 
     function addGroup() {
         const _groupName = prompt("Enter your input:");
@@ -65,7 +70,9 @@
     }
 
     function updateLinesBySet(_groups) {
-        matrix_map = [];
+        // matrix_map = [];
+        flattened_map = [];
+        grouped_map = [];
         linesBySet = {};
         if( !mounted || _groups.length == 0 ) return;
 
@@ -100,10 +107,27 @@
         linesBySet = _ns;
     }
 
+    function getSetValues(_set_name) {
+        let _set_values = availableSets.find(set => set.name === _set_name);
+        if( !_set_values ) null;
+        return _set_values;
+    }
+
+    function getSetUsabeLen(_set_name) {
+        let _set_values = availableSets.find(set => set.name === _set_name);
+        if( !_set_values ) null;
+        return _set_values.len - _set_values.default_loss;
+    }
+
     function calculate(){
         Object.keys(linesBySet).forEach(set_key => {
             let _lineItems = linesBySet[set_key];
-            performCalc(_lineItems);
+            let _set = getSetValues(set_key);
+            if( !_set ) {
+                alert('Set not found');
+                return;
+            };
+            performCalc(_lineItems, _set);
         });
     }
 
@@ -111,69 +135,180 @@
         if( !Array.isArray(_lineItems) || _lineItems.length == 0 ) return;
 
         const usable_len = _set.len - _set.default_loss;
+
+        // check if all lines fit in the usable_len
         let _map = [];
-        // perform matrix calculation on _lineItems
-        for ( let row = 0; row < _lineItems.length; row++ ){
-            for( let col = 0; col < _lineItems.length; col++ ) {
-                if( row == col ){
-                    // manage diagonal
-                    let _len = _lineItems[row].tot_len; // use the len that includes the cut loss
-                    let _obj = {
-                        included_lines: [_lineItems[row].name],
-                        fits: _len <= usable_len,
-                        used_len: _len,
-                        leftover: usable_len - _len,
-                    }
-                    if(!_map[row]) _map[row] = [];
-                    _map[row][col] = _obj;
-                }
+        let _tot_len = _lineItems.reduce((acc, lineItem) => acc + lineItem.tot_len, 0);
+        console.log('_tot_len', _tot_len, usable_len);
+        if( _tot_len <= usable_len ) {
+            // all lines fit in the usable_len;
+            let _obj = {
+                included_lines: _lineItems.map(lineItem => lineItem.name),
+                fits: true,
+                used_len: _tot_len,
+                leftover: usable_len - _tot_len,
+            }
+            
+            _map = [_obj];
+        } else if ( _lineItems.length == 2 ){
+            let _l1 = {
+                included_lines: [_lineItems[0].name],
+                fits: _lineItems[0].tot_len <= usable_len,
+                used_len: _lineItems[0].tot_len,
+                leftover: usable_len - _lineItems[0].tot_len,
+            }
+            let _l2 = {
+                included_lines: [_lineItems[1].name],
+                fits: _lineItems[1].tot_len <= usable_len,
+                used_len: _lineItems[1].tot_len,
+                leftover: usable_len - _lineItems[1].tot_len,
+            }
 
-                if( row < col ){
-                    // manage upper triangle
-                    let _len = 0;
-                    let _lines = [];
-                    if( col > (row +1) ) {
-                        console.log('col > (row +1)', _lineItems[col-1], _lineItems[col]);
-                        _len = _map[row][col-1].used_len + _lineItems[col].tot_len; 
-                        _lines = [..._map[row][col-1].included_lines, _lineItems[col].name];
-                    } else {
-                        _len = _lineItems[row].tot_len + _lineItems[col].tot_len; // use the len that includes the cut loss
-                        _lines = [_lineItems[row].name, _lineItems[col].name];
-                    }
+            _map = [_l1, _l2];
+        } else if ( _lineItems.length > 2 ) {
+            // the max combos are _lineItems.length - 1 ( if all lines fits on one bar not entering this loop)
+            let max_combos = _lineItems.length - 1;
+            // max_combos = 2;
+            for (let current_combo = 2; current_combo <= max_combos; current_combo++) {
+                console.log('current_combo', current_combo, max_combos);
+                
+                let _list = calculateCombinations(_lineItems, current_combo);
+                console.log('_list', _list);
+                if(!_list || _list.length == 0) continue;
+                
+                for( let _ind = 0; _ind < _list.length; _ind++ ){
+                    let listItems = _list[_ind];
+                    console.log('listItems', listItems);
+                    let _l1 = listItems[0];
                     let _obj = {
-                        included_lines: _lines,
-                        fits: _len <= usable_len,
-                        used_len: _len,
-                        leftover: usable_len - _len,
+                        included_lines: [_l1.name],
+                        fits: _l1.tot_len <= usable_len,
+                        used_len: _l1.tot_len,
+                        leftover: usable_len - _l1.tot_len,
                     }
-                    if(!_map[row]) _map[row] = [];
-                    _map[row][col] = _obj;
+                    for (let _li= 1; _li < listItems.length; _li++ ){
+                        let _l = listItems[_li];
+                        let _tot_len = _obj.used_len + _l.tot_len;
+                        _obj['included_lines'] = [..._obj.included_lines, _l.name];
+                        _obj['fits'] = _tot_len <= usable_len;
+                        _obj['used_len'] = _tot_len;
+                        _obj['leftover'] = usable_len - _tot_len;
+                    }
+        
+                    _map.push(_obj);
+                   
                 }
-
-                if( row > col ){
-                    // manage lower triangle
-                    let _len = 0;
-                    let _lines = [];
-                    if( col > 0) {
-                        _len = _map[row][col-1].used_len + _lineItems[col].tot_len; 
-                        _lines = [..._map[row][col-1].included_lines, _lineItems[col].name];
-                    } else {
-                        _len = _lineItems[row].tot_len + _lineItems[col].tot_len; // use the len that includes the cut loss
-                        _lines = [_lineItems[row].name, _lineItems[col].name];
-                    }
-
-                    let _obj = {
-                        included_lines: _lines,
-                        fits: _len <= usable_len,
-                        used_len: _len,
-                        leftover: usable_len - _len,
-                    }
-                    if(!_map[row]) _map[row] = [];
-                    _map[row][col] = _obj;
+            } 
+            // do the 1 elem combo
+            for (let _li= 0; _li < _lineItems.length; _li++ ){
+                let _l = _lineItems[_li];
+                let _obj = {
+                    included_lines: [_l.name],
+                    fits: _l.tot_len <= usable_len,
+                    used_len: _l.tot_len,
+                    leftover: usable_len - _l.tot_len,
                 }
+                _map.push(_obj);
             }
         }
-        matrix_map = _map;
+
+        console.log('_map', _map);
+        flattened_map = _map;
+        
+        sortInGroups(_lineItems);
+        // flatMap(_map, _lineItems);
+    }
+
+    //
+    function sortInGroups( _lineItems){
+        
+        let flattened_map_ordered = flattened_map.sort((a, b) => {
+            return b.included_lines.length - a.included_lines.length;
+        });
+
+        //
+        grouped_map = [];
+        let grouped = [];
+        flattened_map_ordered.forEach((elem, index) => {
+            if( elem.fits == false ) return;
+            if( grouped.length == 0){
+                grouped.push(
+                    {
+                        all_lines: elem.included_lines,
+                        elements: [elem],
+                    }
+                );
+            } else {
+                let included = false;
+                grouped.forEach(group => {
+                    if( fitsInGroup(elem, group) ){
+                        group.all_lines = [...group.all_lines, ...elem.included_lines];
+                        group.elements.push(elem);
+                        included = true;
+                    }
+                });
+
+                if( !included ){
+                    grouped.push(
+                        {
+                            all_lines: elem.included_lines,
+                            elements: [elem],
+                        }
+                    );
+                }
+            }
+        });
+
+        // filter out groups that do no cover all lines
+        grouped = grouped.filter(group => group.all_lines.length == _lineItems.length);
+
+        grouped_map = grouped;
+
+    }
+
+    function fitsInGroup(elem, group){
+        let itersects = group.all_lines.filter(line => elem.included_lines.includes(line));
+        return itersects.length == 0;
+    }
+
+    // function calculateCoordinates(_required_lines, i, _lineItemsLength){
+    //     // 1 element plus extra required_lines elements, in case of current_combo == 2, 1 element plus 1 extra element 
+    //     if( _required_lines > _lineItemsLength - 1 ) return [];
+
+    //     coordinates = [];
+    //     if( i == 0 ){
+    //         for (let j= i+1; j <= _required_lines; j++ ){
+    //             coordinates.push(j);
+    //         }
+    //     } else if ( i == _lineItemsLength - 1 ){
+    //         // in last position
+    //         for (let j= i-1; j >= i-_required_lines; j-- ){
+    //             coordinates.push(j);
+    //         } 
+    //     } else {
+    //         // in the middle 
+
+    //     }
+    // }
+    function calculateCombinations(elements, k){
+
+        const result = [];
+
+        function backtrack(temp, start) {
+            if (temp.length === k) {
+            result.push(temp.slice());
+            return;
+            }
+
+            for (let i = start; i < elements.length; i++) {
+                temp.push(elements[i]);
+                backtrack(temp, i + 1);
+                temp.pop();
+            }
+        }
+
+        backtrack([], 0);
+        return result;
     }
 
     onMount(() => {
@@ -266,6 +401,7 @@
                                 <th class="text-left px-1">Len</th>
                                 <th class="text-left px-1">Cut Loss</th>
                                 <th class="text-left px-1">Tot len</th>
+                                <th class="text-left px-1">leftOver</th>
                             </tr>
                             {#each linesBySet[setGroupKey] as lineItem}
                                 <tr>
@@ -274,6 +410,7 @@
                                     <td class="px-1">{lineItem.len} mm</td>
                                     <td class="px-1">{lineItem.tot_cut_loss} mm</td>
                                     <td class="px-1">{lineItem.tot_len} mm</td>
+                                    <td class="px-1">{getSetUsabeLen(setGroupKey)-lineItem.tot_len} mm</td>
                                 </tr>
                             {/each}
                         </table>
@@ -283,22 +420,75 @@
         </div>
     </div>
     <div>
-        <h3 class="mt-2 mb-2">Calculation map</h3>
+        <h3 class="mt-2 mb-2">Calculation</h3>
         <div>
             <button on:click={() => calculate()}>calc</button>
         </div>
-        <div>
+        <!-- <div>
             <table>
                 {#each matrix_map as row, row_index}
                     <tr>
                         {#each row as col, col_index}
-                            <td class="px-1 py-1 cell-bg-{ col.fits ? 'success':'danger' } ">
-                                { col.included_lines.join(',') } [{col.leftover}mm]
-                            </td>
+                            {#if !col}
+                                <td></td>
+                            {:else}
+                                <td class="px-1 py-1 cell-bg-{ col.fits ? 'success':'danger' } ">
+                                    { col.included_lines.join(',') } [ 
+                                        <span class="">{col.used_len}</span>/
+                                        <span class="text-danger">{col.leftover}</span>
+                                    ]mm
+                                </td>
+                            {/if}
                         {/each}
                     </tr>
                 {/each}
             </table>
+        </div> -->
+        <div>
+            {#if flattened_map.length > 0}
+                <div>
+                    <h3 class="mt-2 mb-2"> List</h3>
+                </div>      
+                <div>
+                    <table>
+                        {#each flattened_map as elem }
+                            <tr>
+                                <td class="px-1 py-1 cell-bg-{ elem.fits ? 'success':'danger' } ">
+                                    { elem.included_lines.join(',') } [ 
+                                        <span class="">{elem.used_len}</span>/
+                                        <span class="text-danger">{elem.leftover}</span>
+                                    ]mm
+                                </td>
+                            </tr>
+                        {/each  }
+                    </table>
+                            
+                </div>
+            {/if}
+        </div>
+        <div>
+            {#if grouped_map.length > 0}
+                <div>
+                    <h3 class="mt-2 mb-2"> List</h3>
+                </div>      
+                <div>
+                    <table>
+                        {#each grouped_map as group }
+                            <tr>
+                                {#each group.elements as elem}
+                                    <td class="px-1 py-1 cell-bg-success">
+                                        { elem.included_lines.join(',') } [ 
+                                            <span class="">{elem.used_len}</span>/
+                                            <span class="text-danger">{elem.leftover}</span>
+                                        ]mm
+                                    </td>
+                                {/each}
+                            </tr>
+                        {/each  }
+                    </table>
+                            
+                </div>
+            {/if}
         </div>
     </div>
 </div>
